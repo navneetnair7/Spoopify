d2afa32408ef6167f7825228402628321def5f55
navneetnair7 <navneetnair95@gmail.com>
2023-07-09 22:34:27 +0530

    Started listing songs

---
+++ components/SongItem.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import useLoadIamge from "@/hooks/useLoadIamge";
+import { Song } from "@/types";
+import Image from "next/image";
+import PlayButton from "./PlayButton";
+
+interface SongItemProps {
+  data: Song;
+  onClick: (id: String) => void;
+}
+
+const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
+  const imagePath = useLoadIamge(data);
+  return (
+    <div
+      onClick={() => onClick(data.id)}
+      className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3"
+    >
+      <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
+        <Image
+          className="object-cover"
+          src={"/images/liked.png" || imagePath}
+          fill
+          alt="image"
+        />
+      </div>
+      <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+        <p className="font-semibold truncate w-full">{data.title}</p>
+        <p className="text-neutral-400 text-sm pb-2 w-full truncate">
+          By {data.author}
+        </p>
+      </div>
+      <div className="absolute bottom-24 right-5">
+        <PlayButton />
+      </div>
+    </div>
+  );
+};
+
+export default SongItem;
