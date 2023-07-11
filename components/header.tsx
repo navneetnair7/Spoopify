321e691df097f08748afa4af9f6a435e3577c2b5
navneetnair7 <navneetnair95@gmail.com>
2023-07-11 12:36:20 +0530

    Search Feature

--- components/header.tsx
+++ components/header.tsx
@@ -33,7 +33,11 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
   };
 
   return (
-    <div className={twMerge(`h-fit bg-gradient-to-b from-emerald-800 p-6`)}>
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6 ${className}`
+      )}
+    >
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
           <button
